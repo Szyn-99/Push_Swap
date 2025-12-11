10fdd26b9ae305a6ce4e677307ebff23ae859ba7
Aymane El Halkouj <aymel-ha@e1r6p12.1337.ma>
2025-12-11 23:51:48 +0100

    go

--- operations/rotate_reverse.c
+++ operations/rotate_reverse.c
@@ -6,44 +6,45 @@
 /*   By: aymel-ha <aymel-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 23:25:32 by aymel-ha          #+#    #+#             */
-/*   Updated: 2025/12/11 23:25:33 by aymel-ha         ###   ########.fr       */
+/*   Updated: 2025/12/11 23:51:20 by aymel-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void op_rotate(t_list **stack)
+void op_rotate_reverse(t_list **stack)
 {
     if(!stack || !*stack || !(*stack)->next)
         return ;
     t_list *first_node = NULL;
     t_list *last_node = NULL;
-
+    t_list *before_last_node = NULL;
+    
+    before_last_node = *stack;
+    while(before_last_node->next-e)
+        before_last_node = before_last_node->next;
+    before_last_node= NULL;
+    last_node = ft_lstlast(*stack);
     first_node = *stack;
-    *stack = (*stack)->next;
-    first_node->next = NULL;
-
-    last_node = *stack;
-    while(last_node->next)
-        last_node = last_node->next;
+    *stack = last_node;
     last_node->next = first_node;
 }
 
 
 
-void op_rotate_a(t_list **stack_a)
+void op_rotate_reverse_a(t_list **stack_a)
 {
-    op_rotate(stack_a);
+    op_rotate_reverse(stack_a);
     ft_putstring("ra\n");
 }
-void op_rotate_b(t_list **stack_b)
+void op_rotate_reverse_b(t_list **stack_b)
 {
-    op_rotate(stack_b);
+    op_rotate_reverse(stack_b);
     ft_putstring("rb\n");
 }
-void op_rotate_rr(t_list **stack_a, t_list **stack_b)
+void op_rotate_reverse_r(t_list **stack_a, t_list **stack_b)
 {
-    op_rotate(stack_a);
-    op_rotate(stack_b);
+    op_rotate_reverse(stack_a);
+    op_rotate_reverse(stack_b);
     ft_putstring("rr\n");
 }
